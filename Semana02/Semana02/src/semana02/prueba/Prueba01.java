7efb583c1d99a98a382368c42d339efa8fb5662b
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-04-12 08:13:53 -0500

    Semana 02

---
+++ Semana02/Semana02/src/semana02/prueba/Prueba01.java
@@ -0,0 +1,18 @@
+package semana02.prueba;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @email gcoronelc@gmail.com
+ * @blog http://www.desarrollasoftware.com/
+ * @youtube https://www.youtube.com/DesarrollaSoftware
+ * @cursos https://gcoronelc.github.io/
+ * @facebook https://www.facebook.com/groups/desarrollasoftware/
+ */
+public class Prueba01 {
+	
+	public static void main(String[] args) {
+		int n = 6 + 2 * 3 - 5/2;
+		System.out.println("n = " + n);
+	}
+
+}
