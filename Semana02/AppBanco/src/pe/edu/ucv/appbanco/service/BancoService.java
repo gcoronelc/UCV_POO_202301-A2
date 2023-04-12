8fe151c4baea71fe17473234c9396396e63f9932
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-04-12 08:36:55 -0500

    Semana 02

---
+++ Semana02/AppBanco/src/pe/edu/ucv/appbanco/service/BancoService.java
@@ -0,0 +1,13 @@
+package pe.edu.ucv.appbanco.service;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @email gcoronelc@gmail.com
+ * @blog http://www.desarrollasoftware.com/
+ * @youtube https://www.youtube.com/DesarrollaSoftware
+ * @cursos https://gcoronelc.github.io/
+ * @facebook https://www.facebook.com/groups/desarrollasoftware/
+ */
+public class BancoService {
+
+}
